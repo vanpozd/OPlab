d10ab70a640e023e06b077c8bf27aae6cc100636
ivan <Vanjapozd@gmail.com>
2023-03-15 13:43:06 +0200

    mod lasts

--- OP1lab.cpp
+++ OP1lab.cpp
@@ -1,85 +1,127 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+//#include <cstring>
 #include <string>
-#include "Streamlab.h"
+#include <string.h>
+//#include <encryptin.cpp>
 
-int main() 
+int main(int argc,char* argv[])
 {
-    FILE* fpointer;
-    char chartoencrypt;
-	std::string infilename, outfilename, outputText, paroutputText;
-    cout << "Введіть назфу файлу який треба зашифрувати: ";
-	std::getline(std::cin, infilename);
-	outfilename = "encrypted" + infilename;
+	std::string outputText, paroutputText, first_text;
     //open infile
-    std::ifstream inputFile(infilename);
-	if (!inputFile.is_open()) 
+    if(strcmp(*argv, "FileStream"))
     {
-        std::cout << "В створених файлах не знайдено файлу з імʼям: " << infilename << std::endl;
-        std::ofstream inputFile(infilename);
+        std::string infilename, outfilename;
+        std::cout << "Введіть назфу файлу який треба зашифрувати: " << std::endl;
+    	std::getline(std::cin, infilename);
+        std::ifstream inputFile(infilename);
         if (!inputFile.is_open()) 
         {
-            std::cout << "[ERROR] Помилка при створенні файлу: " << infilename << std::endl;
-		}
-        else
-        {
-            std::cout << "Створено файл з імʼям: " << infilename << std::endl;
+            
+            std::cout << "В створених файлах не знайдено файлу з імʼям: " << infilename << std::endl;
+            std::ofstream inputFile(infilename);
+            if (!inputFile.is_open()) 
+            {
+                std::cout << "[ERROR] Помилка при створенні файлу: " << infilename << std::endl;
+            }
+            else
+            {
+                std::cout << "Ведіть текст у файл:" << infilename << std::endl;
+                std::getline(std::cin, first_text);
+                inputFile << first_text;
+            }
         }
-    }
-
-    //open outfile
-    std::ofstream outputFile(outfilename);
-	if (!inputFile.is_open()) 
-    {
-        std::cout << "В створених файлах не знайдено файлу з імʼям: " << outfilename << std::endl;
+        //open outfile
+        inputFile.close();
+        outfilename = "encrypted" + infilename;
         std::ofstream outputFile(outfilename);
-        if (!inputFile.is_open()) 
+        if (!outputFile.is_open()) 
         {
             std::cout << "[ERROR] Помилка при створенні файлу: " << outfilename << std::endl;
-		}
+        }
         else
         {
-            std::cout << "Створено файл з імʼям: " << outfilename << std::endl;
+            std::cout << "Вихідний файл: " << outfilename << std::endl;
         }
+
+        char c;
+        while (inputFile.get(c)) 
+        {
+            if ((int)c % 2 == 0) 
+            {
+                paroutputText = paroutputText + c;
+            }
+            else 
+            {
+                outputText = outputText + c;
+            }
+        }
+        outputText = paroutputText + outputText; 
+        std::cout << outputText << std::endl;
+        outputFile << outputText;
+        outputFile.close();
+        inputFile.close();
+        std::cout << "Текст було успішно зашифровано з файлу " << infilename << " до файлу " << outfilename << std::endl;
+        return 0;
     }
-
-    // reading end encriptin file 
-    char c;
-    while (inputFile.get(c)) {
-        if ((int)c % 2 == 0) {
-            paroutputText = paroutputText + c;
-        }
-        else {
-            outputText = outputText + c;
-        }
-    }
-    outputText = paroutputText + outputText; 
-    std::cout << outputText << std::endl;
-
-    outputFile << outputText;
-    outputFile.close();
-    
-    inputFile.close();
-
-    std::cout << "Текст було успішно зашифровано з файлу " << infilename << " до файлу " << outfilename << std::endl;
-
-    return 0;
-}
-
-void openfileStream(string infilename)
-{	
-	std::ifstream inputFile(infilename);
-	if (!inputFile.is_open()) 
+    else if(strcmp(*argv, "FilePointer"))
     {
-        std::cout << "В створених файлах не знайдено файлу з імʼям: " << infilename << std::endl;
-        std::ofstream inputFile(infilename);
-        if (!inputFile.is_open()) 
+        FILE* inpointer;
+        FILE* outpointer;
+        char infilename[30];
+        char outfilename[30] = "encrypted";
+        std::cout << "Введіть назфу файлу який треба зашифрувати: " << std::endl;
+        scanf("%s", infilename);
+        if ((inpointer = fopen(infilename, "r")) == NULL)
+        {
+            std::cout << "Такго файлу не знайдено.\n" << std::endl;
+            if((inpointer = fopen(infilename, "a")) == NULL)
+            {
+                std::cout << "[ERROR] Помилка при створенні файлу: " << infilename << std::endl;
+            }
+            else
+            {
+                std::cout << "Ведіть текст у файл:" << infilename << std::endl;
+                std::getline(std::cin, first_text);
+                const char* text_buf = first_text.c_str();
+                fprintf(inpointer,"%s",text_buf);
+            }
+        }
+        fclose(inpointer);
+        FILE* inpointer;
+        //open outfile
+        strcat(outfilename, infilename);
+        if((outpointer = fopen(outfilename, "a")) == NULL)
         {
             std::cout << "[ERROR] Помилка при створенні файлу: " << infilename << std::endl;
-		}
+        }
         else
         {
-            std::cout << "Створено файл з імʼям: " << infilename << std::endl;
+            std::cout << "Вихідний файл: " << outfilename << std::endl;
         }
+
+        char c;
+        while ((c = fgetc(inpointer)) != EOF) 
+        { 
+            if (ftell(inpointer) % 2 == 0)
+            {
+                paroutputText = paroutputText + c;
+            }
+            else 
+            {
+                outputText = outputText + c;
+            }
+        }
+        outputText = paroutputText + outputText;
+        const char* text_buf = outputText.c_str();
+        fprintf(outpointer,"%s",text_buf);
+        fclose(inpointer);
+        fclose(outpointer);
+    }
+    else
+    {
+        std::cout << "[ERROR] Такого моду не існує." << std::endl;
+        return 1;
     }
 }
