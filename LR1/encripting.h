d1aad93efa6ffec5648c3bcdef32d0c6737c499e
ivan <Vanjapozd@gmail.com>
2023-03-26 11:37:22 +0300

    DONE

---
+++ LR1/encripting.h
@@ -0,0 +1,9 @@
+#ifndef ENCRIPTING_H
+#define ENCRIPTING_H
+
+#include <string>
+
+std::string encripting_stream(std::string inputstring);
+std::string encripting_pointer(char* inputstring);
+
+#endif
